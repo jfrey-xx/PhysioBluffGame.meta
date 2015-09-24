df5573d5e5c81cd27b7e6c0f8840b1b1e2929ffb
jfrey <nomhad@gmail.com>
2015-09-24 15:06:42 +0200

    init webcam: prepare for kinect2

--- tools/init_webcams.sh
+++ tools/init_webcams.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 
 # load modules
-sudo modprobe v4l2loopback video_nr=4,5,6 card_label="ps eye raw 0","ps eye raw 1","ps eye raw 2" 
+sudo modprobe v4l2loopback video_nr=4,11,12,13 card_label="ps eye raw 0","kinect 2 rgb", "kinect 2 ir", "kinect 2 depth" 
 
 # set v4l2loopback to 30fps
 v4l2-ctl -d /dev/video4 --set-ctrl  sustain_framerate=1
 sudo v4l2loopback-ctl set-fps 30 /dev/video4
-
-v4l2-ctl -d /dev/video5 --set-ctrl  sustain_framerate=1
-sudo v4l2loopback-ctl set-fps 30 /dev/video5
-
-v4l2-ctl -d /dev/video6 --set-ctrl  sustain_framerate=1
-sudo v4l2loopback-ctl set-fps 30 /dev/video6
 
 
 # Enable raw mode and set frame rate for ps eye, setup exposure / focus mode / flip / light param
@@ -36,20 +30,3 @@ v4l2-ctl -d /dev/video1 --set-parm=30 \
   --set-ctrl gain_automatic=0 \
   --set-ctrl gain=$GAIN
 
-v4l2-ctl -d /dev/video2 --set-parm=30 \
-  --set-ctrl focus_automatic_continuous=1 \
-  --set-ctrl horizontal_flip=0 \
-  --set-ctrl vertical_flip=0 \
-  --set-ctrl auto_exposure=1 \
-  --set-ctrl exposure=255 \
-  --set-ctrl gain_automatic=0 \
-  --set-ctrl gain=$GAIN
-
-v4l2-ctl -d /dev/video3 --set-parm=30 \
---set-ctrl focus_automatic_continuous=1 \
---set-ctrl horizontal_flip=0 \
---set-ctrl vertical_flip=0 \
---set-ctrl auto_exposure=1 \
---set-ctrl exposure=255 \
---set-ctrl gain_automatic=0 \
---set-ctrl gain=$GAIN
