6cac890ce7d7896355c7f42a0518ba06165c849a
jfrey <nomhad@gmail.com>
2015-09-24 16:39:50 +0200

    launching acquisition for 4 diffent cams

--- tools/launch_video_acquisition.sh
+++ tools/launch_video_acquisition.sh
@@ -15,14 +15,22 @@ TARGET_COM="python main.py"
 SLEEPY=4
 
 # parameters for each instance
-PARAMS_1="--webcam 4 --user-id 0 --algo 1"
+
+# logitech RGB
+PARAMS_1="--webcam 0 --user-id 0 --algo 1"
 DESKTOP_1="1"
 
-PARAMS_2="--webcam 5 --user-id 1 --algo 1"
+# ps eye raw
+PARAMS_2="--webcam 2 --user-id 1 --algo 1"
 DESKTOP_2="2"
 
-PARAMS_3="--webcam 6 --user-id 2 --algo 1"
+# kinect RGB
+PARAMS_3="--webcam 3 --user-id 2 --algo 1"
 DESKTOP_3="3"
+
+# kinkect IR
+PARAMS_4="--webcam 4 --user-id 3 --algo 0"
+DESKTOP_3="4"
 
 # remember current desktop to go back here after
 # FIXME: may not work with a *vast* amount of desktop
@@ -30,9 +38,9 @@ CURRENT_DESKTOP=`wmctrl -d | grep "*" | head -c 1`
 echo "Current desktop: $CURRENT_DESKTOP"
 
 echo "Lauching soft 1 in desktop $DESKTOP_1"
-wmctrl -s $DESKTOP_1
+#wmctrl -s $DESKTOP_1
 $BINARY "cd $TARGET_FOLDER && $TARGET_COM $PARAMS_1" > /dev/null 2>&1 &
-sleep $SLEEPY
+#sleep $SLEEPY
 
 # dead code, not used in fact, but maybe in the future...
 # could not use $! since child spawn another process
@@ -41,17 +49,22 @@ sleep $SLEEPY
 #echo "Got PID $PID_1"
 
 echo "Lauching soft 2 in desktop $DESKTOP_2"
-wmctrl -s $DESKTOP_2
+#wmctrl -s $DESKTOP_2
 $BINARY "cd $TARGET_FOLDER && $TARGET_COM $PARAMS_2" > /dev/null 2>&1 &
-sleep $SLEEPY
+#sleep $SLEEPY
 
 echo "Lauching soft 3 in desktop $DESKTOP_3"
-wmctrl -s $DESKTOP_3
+#wmctrl -s $DESKTOP_3
 $BINARY "cd $TARGET_FOLDER && $TARGET_COM $PARAMS_3" > /dev/null 2>&1 &
-sleep $SLEEPY
+#sleep $SLEEPY
 
-echo "Go back to desktop $CURRENT_DESKTOP"
-wmctrl -s $CURRENT_DESKTOP
+echo "Lauching soft 4 in desktop $DESKTOP_4"
+#wmctrl -s $DESKTOP_4
+$BINARY "cd $TARGET_FOLDER && $TARGET_COM $PARAMS_4" > /dev/null 2>&1 &
+#sleep $SLEEPY
+
+#echo "Go back to desktop $CURRENT_DESKTOP"
+#wmctrl -s $CURRENT_DESKTOP
 
 # An infinite loop that will take all child processes with it if breaks
 while :
