2d1a3ae9f3ca5ccdfe451e0e4e2f7692011b1d86
jfrey <nomhad@gmail.com>
2015-09-28 20:57:17 +0200

    Nicer plot.

--- stats/analyze_BPM.R
+++ stats/analyze_BPM.R
@@ -15,7 +15,7 @@ data_path = "~/bluff_game/data_validation/results/";
 output_folder = data_path
 output_plot_folder = paste(data_path, 'figs/', sep="");
 
-tag <- "BPM"
+tag <- "PPG_BPM"
 
 # sampling rate of the data
 srate <- 8
@@ -42,10 +42,20 @@ names(data)[names(data)=="subject"] <- "session"
 
 # rename some devices
 levels(data$device)[levels(data$device)=="BPM"] <- "ECG"
-levels(data$device)[levels(data$device)=="Logitech"] <- "LogitechC270"
+levels(data$device)[levels(data$device)=="Logitech"] <- "Logitech_C270"
+levels(data$device)[levels(data$device)=="PSEyeRaw"] <- "PSEye_Raw"
+levels(data$device)[levels(data$device)=="Kinect2RGB"] <- "Kinect2_RGB"
+levels(data$device)[levels(data$device)=="Kinect2IR"] <- "Kinect2_IR"
+
+# reverse order so as we got ps eye, logitech, kinect rgb, kinect ir
+data$device=factor(data$device,rev(levels(data$device)))
 
 # discard data after cutofftime
 data <- data[data$time<=600,]
+
+# make factor of session + rename
+data$session <- factor(data$session)
+levels(data$session) <- paste("Session", levels(data$session), sep="_")
 
 # set a time across sessions
 data$n_across <- NA
@@ -62,7 +72,7 @@ averageWindow <- 1;
   
 data$av <- NA
 for (dev in levels(data$device)) {
-  for (s in unique(data$session)) {
+  for (s in levels(data$session)) {
     dataSelect <- data[data$device == dev & data$session == s,]
    
     #Make zoo object of data
@@ -74,32 +84,38 @@ for (dev in levels(data$device)) {
   }
 }
 
+## set ECG reference everywhere
 
-## plot across time, one for each PPG device
+# will ease later side-by-side plots
+data$ref_av <- NA
+data$ref_HR <- NA
+for (s in levels(data$session)) {
+  dataRef <- data[data$device == "ECG" & data$session == s,]
+  for (dev in levels(data$device)) {
 
-# color solarized version, red, violet, blue, cyan, green
-solConstruct <- c("#dc322f","#6c71c4", "#268bd2", "#2aa198", "#859900")
-# base2, base3, ...and something darker
-solSession <- c("#eee8d5", "#fdf6e3", "#ded8c7")
-
-ppg_devices <- unique(data[data$construct == "ppg",]$device)
-
-for (dev in ppg_devices) {
-  # retrieve PPG device + ECG reference
-  dataSelect <- data[data$device == dev | data$construct == "ecg",]
-  
-  splot <- ggplot(data=dataSelect, aes(x = time, y = av, color=device))  + 
-    geom_line(alpha=0.75) +
-    # let's free the Y axis! (in case one channel goes bersek)... but less meaningful in regular scenarios
-    facet_wrap(~ session, scales = "free_y", ncol = 1)
-    #scale_colour_manual(values=solConstruct)  + 
-  ggsave(path=output_plot_folder , filename=paste(tag, "_", dev, "_BPM.pdf",sep=""), plot=splot, width=10, height=7)
-    
+    data[data$device == dev & data$session == s,]$ref_av <- dataRef$av
+    data[data$device == dev & data$session == s,]$ref_HR <- dataRef$HR
+  }
 }
 
+## plot across time, one panel for each PPG device
+
+dataPPG <- data[data$construct == "ppg",]
+splot <- ggplot(data=dataPPG, aes(x = time, y = av))  + 
+    # ref ECG, base02 color
+    geom_line(aes(y = ref_av), color="#073642", alpha=0.66) +
+    geom_line(aes(color=device), alpha=0.66) +
+    # let's free the Y axis! (in case one channel goes bersek)... but less meaningful in regular scenarios
+    facet_grid(device ~ session) +
+    # remove legend for device
+    guides(colour=FALSE)+
+    # labels
+    labs(x = "Time (s)", y="Heart rate (BPM)") +
+    # remove margins, we need max space + increase font size
+    theme(plot.margin = unit(c(0,0,0,0), "cm"), text = element_text(size=20))
+ggsave(path=output_plot_folder , filename=paste(tag, ".pdf",sep=""), plot=splot, width=14, height=7)
 
       
-
 
 sink()
 
